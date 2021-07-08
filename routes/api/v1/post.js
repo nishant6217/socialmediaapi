223e93a680a186d65bd5cafbcb12111d9aa32b2e
Nishant Kumar <me.nishant6217@gmail.com>
2021-07-08 12:32:40 +0530

    make some correction so that used in project

--- routes/api/v1/post.js
+++ routes/api/v1/post.js
@@ -10,10 +10,12 @@ const postApi = require('../../../controllers/api/v1/posts_api')
 
 
 
-router.get('/',passport.authenticate('jwt' , {session : false}), postApi.getAllPost);
+router.get('/', postApi.getAllPost);
 router.post('/create' ,passport.authenticate('jwt' , {session : false}),postApi.create);
 
 router.post('/:id/delete' ,passport.authenticate('jwt' , {session : false}),postApi.delete);
 router.post('/:id/update' ,passport.authenticate('jwt' , {session : false}),postApi.update);
 
 module.exports = router;
+
+// ,passport.authenticate('jwt' , {session : false})
